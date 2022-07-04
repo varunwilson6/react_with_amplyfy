70936b36cf2758c5db4d8c62f8960be9631b673f
varunw92 <varun@neoito.com>
2022-07-04 16:26:05 +0530

    auto deploy change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App Pushed to Dev branch
         </p>
         <a
           className="App-link"
